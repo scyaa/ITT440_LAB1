10e3e857af83857af634d34cb3c6829f35c70a6f
scyaa <2020861862@isiswa.uitm.edu.my>
2021-04-17 08:25:24 +0000

    adding my second file

---
+++ userinput.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+/*This is my second program in C*/
+int age;
+
+printf("Hi, how old are you?");
+scanf("%d", &age);
+printf("You are %d years old", age);
+
+return 0;
+
+}
